4d5b809c5de74e43d1d924d305abc688fc04bd04
Guilherme Athayde <guiathayde303@gmail.com>
2023-03-23 18:55:06 -0300

    Setup ESLint and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
